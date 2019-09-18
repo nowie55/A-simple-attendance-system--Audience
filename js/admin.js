a5c2283062164fac093fe068c9362c6dba72bec3
nowie55 <enofestus.2010@gmail.com>
2019-09-18 23:07:21 +0100

    Added delete functionality

--- js/admin.js
+++ js/admin.js
@@ -1,18 +1,30 @@
  var users = [];
- var container ;
+ var container;
  
- function create()
- {
+function create() {
     var firstName = $("#firstName").val()
     var lastName = $("#lastName").val()
     var email = $("#email").val() 
     var department = $("#department").val()
-        console.log("firstName, lastName, email, department ", firstName, lastName, email, department);
-            users.push({id: users.length+1, firstName: firstName,  lastName: lastName, email: email, department: department})
-        populateTable(users);
-        
-            
-    }
+    console.log("firstName, lastName, email, department ", firstName, lastName, email, department);
+    populateTable(users);    
+}
+
+$('.employee-table').on('click', '.delete', function() {
+    var $this = $(this);
+    var id = $this.parent().siblings('.row-id');
+    var idValue = id.text();
+    $.ajax({
+        method: 'DELETE',
+        url: 'http://localhost:3000/users/' + idValue,
+        success: function(result) {
+            console.log(result);
+        }
+    })
+    alert('Employee with id ' + idValue + ' deleted');
+})
+
+
 
 
     function update(){
@@ -33,32 +45,22 @@
                }); 
         }
         
-        function populateTable(data)
-        {
+        function populateTable(data) {
             document.getElementById('body').innerHTML = " ";
                 for(let i = 0; i < users.length; i++){
                     container = document.createElement("tr");
                     container.innerHTML = `
-                                               <td>${data[i]['id']}</td>
+                                               <td class="row-id">${data[i]['id']}</td>
                                                <td>${data[i]['firstName']}</td>
                                                <td>${data[i]['lastName']}</td>
                                                <td>${data[i]['email']}</td>
                                                <td>${data[i]['department']}</td>
                                                <td>
                                                <button class="btn btn-sm btn-primary" data-toggle="modal"onClick="eno()" data-target="#employeeModal">Edit</button>
-                                               <button class="btn btn-sm btn-danger " >Delete</button>
+                                               <button class="btn btn-sm btn-danger delete">Delete</button>
                                              </td> 
                                                 </tr>`;
                     document.getElementById('body').appendChild(container);
                 }
 
-        }
-
-        $("").onClick(function(){
-
-
-        })
-        function eno(){
-            
-alert('boomshakala')
         }
